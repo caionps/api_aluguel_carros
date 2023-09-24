43ba7c51a2fe588598c0a0d5e44abd9e22e8b8d8
caionps <caio.nepos@gmail.com>
2023-09-24 13:39:03 -0300

    método get finalizo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from datetime import date
+import json
 import csv
 import pandas as pd
 
@@ -18,28 +19,28 @@ def validar_cpf(cpf):
 # Aqui se encontra uma função de continuação que utiliza as funções de validação de data e cpf para validar a nova entrada de dados
 def validar_novo_aluguel(dados):
     # SUBSTITUIR TODOS OS CAMPOS Q SÃO OBRIGATÓRIOS POR UMA FUNÇÃO LAMBDA COM LIST COMPREHENSION
-    if not dados.get("dt_inicio_aluguel"):
-        raise ValueError("Data de início do aluguel é obrigatória")
-    if not dados.get("dt_fim_aluguel"):
-        raise ValueError("Data de fim do aluguel é obrigatória")
-    if not dados.get("km_rodados_aluguel"):
-        raise ValueError("É necessário indicar a rodagem do carro no aluguel")
-    if not dados.get("carro"):
-        raise ValueError("Carro é obrigatório")
-    if not dados.get("nm_cliente"):
-        raise ValueError("Nome do cliente é obrigatório")
-    if not dados.get("cpf_cliente"):
-        raise ValueError("CPF do cliente é obrigatório")
-    if not dados.get("valor"):
-        raise ValueError("Valor é obrigatório")
-    if validar_data(dados.get("dt_inicio_aluguel")) == False:
-        raise ValueError("O formato da data de início está incorreta")
-    if validar_data(dados.get("dt_fim_aluguel")) == False:
-        raise ValueError("O formato da data final está incorreta")
-    if dados.get("dt_inicio_aluguel") > dados.get("dt_fim_aluguel"):
-        raise ValueError("A data de início do aluguel não pode depois da data de fim do aluguel")
-    if validar_cpf(dados.get("cpf_cliente")) == False:
-        raise ValueError("O CPF está incorreto")
+    if not dados.get('dt_inicio_aluguel'):
+        raise ValueError('Data de início do aluguel é obrigatória')
+    if not dados.get('dt_fim_aluguel'):
+        raise ValueError('Data de fim do aluguel é obrigatória')
+    if not dados.get('km_rodados_aluguel'):
+        raise ValueError('É necessário indicar a rodagem do carro no aluguel')
+    if not dados.get('carro'):
+        raise ValueError('Carro é obrigatório')
+    if not dados.get('nm_cliente'):
+        raise ValueError('Nome do cliente é obrigatório')
+    if not dados.get('cpf_cliente'):
+        raise ValueError('CPF do cliente é obrigatório')
+    if not dados.get('valor'):
+        raise ValueError('Valor é obrigatório')
+    if validar_data(dados.get('dt_inicio_aluguel')) == False:
+        raise ValueError('O formato da data de início está incorreta')
+    if validar_data(dados.get('dt_fim_aluguel')) == False:
+        raise ValueError('O formato da data final está incorreta')
+    if dados.get('dt_inicio_aluguel') > dados.get('dt_fim_aluguel'):
+        raise ValueError('A data de início do aluguel não pode depois da data de fim do aluguel')
+    if validar_cpf(dados.get('cpf_cliente')) == False:
+        raise ValueError('O CPF está incorreto')
     return dados
 
 # MONAD PARA COLOCAR LISTA EM CAIXA ALTA
@@ -57,8 +58,11 @@ def abre_csv():
 def id_aux():
     df = abre_csv()
     def gerar_id():
-        id_aluguel = df['id_aluguel'].max()
-        novo_id_aluguel = id_aluguel + 1
+        max_id_aluguel = df['id_aluguel'].max()
+        if max_id_aluguel is None:
+            novo_id_aluguel = 1
+        else:
+            novo_id_aluguel = max_id_aluguel + 1
         return novo_id_aluguel
     return gerar_id()
 
@@ -73,7 +77,10 @@ def verificar_id(id_de_busca):
                 return True
             return False
         except FileNotFoundError:
-            return False 
+            return False
+        
+def criar_json(chaves,valores):
+    return dict(zip(chaves,valores))
 
 @app.route('/', methods=['POST'])
 def create():
@@ -89,9 +96,14 @@ def create():
 
 @app.route('/', methods=['GET'])
 def read():
-#    json = request.get_json()   
-#    if json.get('id') = 'todos'
-        return 
+    json_id = request.get_json()
+    id_aluguel = json_id.get('id')   
+    lista_id = abre_csv().loc[abre_csv()['id_aluguel'] == id_aluguel]
+    lista_colunas = list(abre_csv().columns)
+    chaves = [chave for chave in lista_colunas]
+    json_resposta = criar_json(chaves,lista_id)
+    
+    return json.dumps(json_resposta)
 
 
 @app.route('/', methods=['PUT'])
